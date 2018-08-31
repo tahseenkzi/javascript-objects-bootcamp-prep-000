c5cbd66c0470959ac824d69b173e46288888a7af
Tahseen Kazi <myronintro@gmail.com>
2018-08-31 02:07:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { 'Drake' : 'Forever' };
 
 function updatePlaylist(playlist, artist, song){
-  return object.assign({}, playlist, {artist : [song]});
+  return object.assign({}, playlist, {artist : song});
 }
 
